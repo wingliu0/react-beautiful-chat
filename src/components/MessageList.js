23240a194ca718d9106833e03d86eab089e6adc1
Wing Liu <wing@cxinno.tw>
2019-05-22 03:25:21 +0800

    add shouldScrollToBottom condition to messageList

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,12 +4,29 @@ import Message from './Messages'
 class MessageList extends Component {
 
   componentDidUpdate(prevProps, prevState) {
-    this.scrollList.scrollTop = this.scrollList.scrollHeight;
+    const shouldScrollToBottom = this.shouldScrollToBottom()
+    if (shouldScrollToBottom) {
+      this.scrollList.scrollTop = this.scrollList.scrollHeight;
+    }
   }
 
-  render () {
+  shouldScrollToBottom() {
+    return this.props.alwaysScrollToBottom || (this.scrollList.scrollTop > this.scrollList.scrollHeight - 600)
+  }
+
+  handleScroll(e) {
+    if (e.target.scrollTop === 0) {
+      // console.log('scrollToTop');
+      // this.$emit('scrollToTop')
+    }
+    if (typeof this.props.onScroll === 'function') {
+      this.props.onScroll(this.scrollList)
+    }
+  }
+
+  render() {
     return (
-      <div className="sc-message-list" ref={el => this.scrollList = el} onScroll={() => this.props.onScroll(this.scrollList)}>
+      <div className="sc-message-list" ref={el => this.scrollList = el} onScroll={(e) => this.handleScroll(e)}>
         {this.props.messages.map((message, i) => {
           return <Message
             message={message}
